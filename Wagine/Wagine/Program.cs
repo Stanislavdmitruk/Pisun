567803ddd964f06b799f1758b244649a1f5a2ee4
BlackStas <mr.stas.klishin@gmail.com>
2025-01-15 11:49:12 +0100

    Add name

--- Wagine/Wagine/Program.cs
+++ Wagine/Wagine/Program.cs
@@ -1,2 +1,22 @@
-﻿int number = 0;
-Console.WriteLine($"Enter number:{number}");
+﻿var names = new List<string>(){"Stas","Oleg","Alina" };
+
+ShowNames(names);
+
+names.Add("Damian");
+
+ShowNames(names);
+
+void ShowNames(List<string> names1)
+{
+    foreach (var name in names1)
+    {
+        if (name == "Alina")
+        {
+            Console.WriteLine($"{name} nie pidor");
+        }
+        else
+        {
+            Console.WriteLine($"{name} pidor");
+        }
+    } 
+}
